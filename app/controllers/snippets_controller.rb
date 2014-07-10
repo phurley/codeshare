c2d7a24dd49fb981f37f6fa6ecf829839a6848f0
Prelang Builder <builder@prelang.com>
2014-07-10 11:41:28 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,4 +1,8 @@
 class SnippetsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_snippet, only: [:show, :edit, :update, :destroy]
 
   # GET /snippets
